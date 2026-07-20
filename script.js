a869c81f3da79a6f2ff346caae9c29114a916f79
ssuyuchini-wedding <tiffany0520326@gmail.com>
2026-07-20 16:34:12 -0500

    fit

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const opening = document.getElementById("opening");
+const hero = document.getElementById("hero");
 function openDoors() {
    opening.classList.add("is-open");
+   setTimeout(() => {
+       opening.style.display = "none";
+       hero.scrollIntoView({
+           behavior: "auto",
+           block: "start"
+       });
+   }, 1450);
 }
 opening.addEventListener("click", openDoors);
